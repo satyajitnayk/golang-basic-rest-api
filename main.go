596f5f0e51f1079e767dc6241bcdb0cf39dcebdb
satyajit nayak <satyajitn15@gmail.com>
2022-07-27 23:51:34 +0530

    Add updateMovie route.

--- main.go
+++ main.go
@@ -62,7 +62,7 @@ func getMovieById(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-func createMovie(w http.ResponseWriter, r *http.Response) {
+func createMovie(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	// create a new variable
 	var movie Movie
@@ -74,6 +74,24 @@ func createMovie(w http.ResponseWriter, r *http.Response) {
 
 	// return created movie
 	json.NewEncoder(w).Encode(movie)
+}
+
+// Logic : first delete from array & craete a new one with updated data
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for index, movie := range movies {
+		if movie.ID == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			var movie Movie
+			_ = json.NewDecoder(r.Body).Decode(&movie)
+			movie.ID = params["id"]
+			movies = append(movies, movie)
+			// return updated movies
+			json.NewEncoder(w).Encode(movie)
+			return
+		}
+	}
 }
 
 func main() {
